cb503d83accff0533174376a30500312e30601b7
Patrick Frazer <pfrazer@chandlersystemsinc.com>
2026-05-18 18:24:43 -0400

    Increase LED intensity to max for use under a white PLA diffuser and add custom colors

--- firmware/include/Config.h
+++ firmware/include/Config.h
@@ -44,9 +44,9 @@ const float AXIS_LIMIT = 350.0;
 
 // RGB LEDs
 const int LED_COUNT = 8;
-const int LED_BRIGHTNESS = 40;
-const unsigned long LED_IDLE_COLOR = 0x00FF00;
-const unsigned long LED_CALIBRATING_COLOR = 0x0000FF;
+const int LED_BRIGHTNESS = 255;
+const unsigned long LED_IDLE_COLOR = 0x2200FF;
+const unsigned long LED_CALIBRATING_COLOR = 0xFFFF00;
 
 // FSM timing
 const long IDLE_SLEEP_TIMEOUT_MS = 2 * 60 * 1000;
